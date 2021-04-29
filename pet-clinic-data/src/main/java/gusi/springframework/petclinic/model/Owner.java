4f2f1cadfee3621eb6fc902fa063defc8e7a1e3d
Ricardo Gusi <ricardogusi@hotmail.com>
2021-04-29 09:40:28 -0300

    pom changes

---
+++ pet-clinic-data/src/main/java/gusi/springframework/petclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package gusi.springframework.petclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ pet-clinic-data/src/main/java/gusi/springframework/petclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package gusi.springframework.petclinic.model;
+
+public class Vet extends Person {
+
+}
